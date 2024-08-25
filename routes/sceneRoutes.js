aa9d8d14a4ffc09da90fbdb4cdf1093c19437c0e
Remote Developer <arwpersonal@gmail.com>
2024-08-25 13:44:47 -0500

    B4 Parts Refactor

--- routes/sceneRoutes.js
+++ routes/sceneRoutes.js
@@ -4,13 +4,12 @@ const sceneController = require('../controllers/sceneController');
 
 router.get('/', sceneController.getAllScenes);
 router.get('/new', sceneController.newScene);
-router.get('/:id/edit', sceneController.getSceneById);
+router.get('/:id', sceneController.getSceneById);
 router.post('/', sceneController.createScene);
 router.post('/:id', sceneController.updateScene);
 router.delete('/:id', sceneController.deleteScene);
 router.get('/:id/play', sceneController.playScene);
 router.post('/:id/execute-step', sceneController.executeStep);
 router.post('/:id/execute', sceneController.executeScene);
-router.get('/light-parts/:characterId', sceneController.getLightParts);
 
 module.exports = router;
