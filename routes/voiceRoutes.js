67f243bd7118444792747fb80ab18452af69a20d
arwpe <arwpersonal@gmail.com>
2024-10-25 14:11:27 -0500

    Working on Replica - close

--- routes/voiceRoutes.js
+++ routes/voiceRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const voiceController = require('../controllers/voiceController');
 
-// Get available voices from Replica API
+// Get available voices
 router.get('/available', voiceController.getAvailableVoices);
 
 // Get voice settings for a character
@@ -14,10 +14,10 @@ router.post('/settings', voiceController.saveVoiceSettings);
 // Generate speech
 router.post('/generate', voiceController.generateSpeech);
 
-// Save to sound library
-router.post('/save-to-library', voiceController.saveToSoundLibrary);
+// Save to library
+router.post('/save-to-library', voiceController.saveToLibrary);
 
 // Save voice preset
-router.post('/preset', voiceController.saveVoicePreset);
+router.post('/preset', voiceController.savePreset);
 
 module.exports = router;
