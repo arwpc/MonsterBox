44f886478b71df5baf55f4d7fe6be15339a6e30c
arwpe <arwpersonal@gmail.com>
2024-10-31 15:34:08 -0500

    rpi

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,6 +3,15 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
+// Configure chai
+chai.config.includeStack = true;
+chai.config.truncateThreshold = 0;
+
+// Make chai and its assertions globally available
+global.chai = chai;
+global.expect = chai.expect;
+global.should = chai.should();
+
 module.exports = {
     chai,
     expect: chai.expect,
